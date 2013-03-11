8c53e36f58560da766f4d28cba883d8fcfc4fa5f
kevinpapst <kpapst@gmx.net>
2013-03-11 19:18:48 +0100

    initial commit

---
+++ library/KepaZendServerApi/BaseApiFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace KepaZendServerApi;
+
+use Zend\ServiceManager\AbstractFactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class BaseApiFactory implements AbstractFactoryInterface
+{
+
+    /**
+     * Determine if we can create a service with name
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param $name
+     * @param $requestedName
+     * @return bool
+     */
+    public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        return false;
+    }
+
+    /**
+     * Create service with name
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param $name
+     * @param $requestedName
+     * @return mixed
+     */
+    public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        return null;
+    }
+}
